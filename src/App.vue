3f8e27c12cd858016dc929979831d1ac5cdee00e
amanoese <amanoese@gmail.com>
2017-08-25 00:56:35 +0900

    viewing google map

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,12 @@
 <template>
-  <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Pastel-Colors</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
-    <div class="container list-group">
-      <div v-for="task in tasks"
-        type="button" class="float-left align-middle list-group-item list-group-item-action">
-        <div class="row">
-          <span class="col-sm-1 col-3 t-center">
-            <toggle-button class="toggle-button" :labels="{checked: 'on', unchecked: 'off'}"/>
-          </span>
-          <span class="mr-auto col-auto t-start" v-text="task"></span>
-          <span class="col-auto  t-start">
-            <a class="btn btn-primary" href="#" role="button">Map</a>
-          </span>
-        </div>
-      </div>
-    </div>
+  <div id="#app">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { alert } from 'vue-strap'
 export default {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      tasks : [
-        '浅草寺',
-        '浅草神社',
-        '本龍院',
-        '今戸神社',
-        '不動院',
-        '石浜神社',
-        '吉原神社',
-        '鷲神社',
-        '矢先稲荷神社'
-      ]
-    }
-  },
-  components: {
-    alert
-  }
 }
 </script>
 
@@ -74,36 +18,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   /* margin-top: 60px; */
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-.t-center {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.t-start {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-}
-.toggle-button {
-  margin-bottom: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,46 @@
 import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+import VueRouter from 'vue-router'
 import ToggleButton from 'vue-js-toggle-button'
 import App from './App.vue'
+import Top from './Top.vue'
 
+Vue.use(VueRouter)
 Vue.use(ToggleButton)
+Vue.use(VueGoogleMaps, {
+  installComponents: true,
+  load: {
+    key: 'google api key',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+  }
+})
 
+Vue.filter('json', x => JSON.stringify(x));
+
+// 3. ルーターインスタンスを作成して、ルートオプションを渡します
+// 追加のオプションをここで指定できますが、
+// この例ではシンプルにしましょう
+const router = new VueRouter({
+  routes : [
+    { path: '/', component: Top }
+  ] // `routes: routes` の短縮表記
+})
+
+// 4. root となるインスタンスを作成してマウントします
+// アプリケーション全体がルーターを認知できるように、
+// ルーターをインジェクトすることを忘れないでください。
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  template: '<App/>',
+  components: { App },
+  watch: {
+    '$route'(to, from) {
+      // Call resizePreserveCenter() on all maps
+      Vue.$gmapDefaultResizeBus.$emit('resize')
+    }
+  }
 })
